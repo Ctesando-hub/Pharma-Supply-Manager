c81e7f6d54e38131e7b4fcb4003c4422b9a6e07b
CarolinaT <27895429@ifts24.edu.ar>
2025-11-09 12:27:04 -0300

    Controller Cliente terminado y probado en Postman

--- Backend/controllers/clientes_controller.js
+++ Backend/controllers/clientes_controller.js
@@ -66,3 +66,67 @@ export const searchClientes = async (req, res) =>{
     }
 };
 
+//Controlador para crear un nuevo cliente
+export const crearCliente = async (req, res) =>{
+    try{
+        const { nombre, telefono, email, direccion, id_ciudad} = req.body; //extraer los datos del body
+
+        // Validamos que sean campos obligatorios
+        if(!nombre || !telefono ||!email || !direccion || !id_ciudad){
+            return res.status(400).json({ message: "Faltan datos obligatorios"});
+        }
+
+
+        //por ahora simulo la insercion con un mensaje
+
+        const nuevoCliente = {
+            id: Date.now(), //simulo un id autoincremental
+            nombre, telefono, email, direccion, id_ciudad
+
+        };
+            console.log("Cliente creado", nuevoCliente);
+
+        res.status(201).json({ message: "Cliente simulado creado correctamente", data: nuevoCliente});       
+
+    } catch (error){
+        res.status(500).json({ message: "Error al crear el cliente simulado", error: error.message});
+    }
+};
+
+//Controlador para actualizar un cliente
+export const actualizarCliente =  async (req, res) => {
+    try{
+        const {id} =  req.params;
+        const {nombre, telefono, email, direccion, id_ciudad } =  req.body;
+
+        //validamos los datos
+        if (!nombre || !telefono ||!email || !direccion || !id_ciudad){
+            return res.status(400).json({message: "Faltan datos obligatorios"});
+        }
+
+        //simulamos la actualizacion
+        const ClienteActualizado = {
+            id, nombre, telefono, email, direccion, id_ciudad
+        };
+
+        res.status(200).json({ message: "Cliente simulado actualizado correctamente", data: ClienteActualizado});
+
+    }catch(error){
+        res.status(500).json({ message: "Error al actualizar Cliente simulado", error: error.message});
+    }
+
+};
+
+//Controlador para eliminar un cliente
+
+export const eliminarCliente =  async (req, res) =>{
+    try{
+        const {id} = req.params;
+
+
+      //simulamos la eliminacion de un producto especifico
+        res.status(200).json({ message: `Cliente simulado con el ID: ${id} eliminado correctamente`});
+    } catch (error){
+        res.status(500).json({ message: "Error al eliminar el cliente", error: error.message});
+    }
+};
--- Backend/routes/clientes_route.js
+++ Backend/routes/clientes_route.js
@@ -2,50 +2,8 @@ import express from "express"; //Importa el modulo Express para crear el servido
 
 const router = express.Router(); //crea un enrutador de express, que permite organizar las rutas en modulos separados.
 
-import {getClientes, getClientesByID, searchClientes} from "../controllers/clientes_controller.js"
+import {crearCliente, getClientes, getClientesByID, searchClientes, actualizarCliente, eliminarCliente} from "../controllers/clientes_controller.js"
 
-let clientes = [
-    {
-    id: 1,
-    nombre: "Juan Pérez",
-    email: "juan.perez@example.com",
-    telefono: "351-4567890",
-    direccion: "Av. Colón 1234, Córdoba",
-    fechaRegistro: "2024-02-15"
-    },
-    {
-    id: 2,
-    nombre: "María López",
-    email: "maria.lopez@example.com",
-    telefono: "351-9876543",
-    direccion: "Bv. San Juan 456, Córdoba",
-    fechaRegistro: "2024-03-22"
-    },
-    {
-    id: 3,
-    nombre: "Carlos Rodríguez",
-    email: "carlos.rodriguez@example.com",
-    telefono: "3543-678912",
-    direccion: "Rivadavia 789, Río Cuarto",
-    fechaRegistro: "2024-05-10"
-    },
-    {
-    id: 4,
-    nombre: "Lucía Fernández",
-    email: "lucia.fernandez@example.com",
-    telefono: "351-1234567",
-    direccion: "Sarmiento 250, Villa María",
-    fechaRegistro: "2024-06-05"
-    },
-    {
-    id: 5,
-    nombre: "Pedro Gómez",
-    email: "pedro.gomez@example.com",
-    telefono: "358-3216549",
-    direccion: "Belgrano 1020, Río Cuarto",
-    fechaRegistro: "2024-07-18"
-    }
-];
 
 //-------------------
 //Route GET - 
@@ -64,57 +22,21 @@ router.get("/:id", getClientesByID)
 //Route POST -Crear nuevo ciente
 //--------------------
 
-router.post("/",(req,res)=>{
-    const {nombre, email, telefono, direccion, fechaRegistro} = req.body; // Extrae las propiedades del cuerpo (body) de la petición.
+router.post("/",crearCliente);
 
-// Validar datos
-    if (!nombre || !email || !telefono || !direccion || !fechaRegistro) {
-        return res.status(400).json({ mensaje: "Faltan datos del cliente" });// Si falta alguno, responde con error 400 (Bad Request).
-    }
-    const nuevoCliente ={  // Crea un nuevo objeto proveedor con un id incremental
-        id: clientes.length +1,
-        nombre,
-        email,
-        telefono,
-        direccion,
-        fechaRegistro
-    };
-    clientes.push(nuevoCliente);   // Agrega el nuevo Cliente al array de clientes existente
-    res.status(201).json({mensaje: "Cliente agregado correctamente", cliente: nuevoCliente});// Devuelve una respuesta 201 (Created) con un mensaje y el nuevo cliente agregado.
-
-});
 
 //-----------------
 //Route PUT -Actualizar datos de un Cliente
 //-----------------
 
-router.put("/:id", (req, res)=>{
-    const id = parseInt(req.params.id); // Extrae el parámetro "id" de la URL y lo convierte a número entero.
-    const index = clientes.findIndex((p) => p.id === id);  
-    
-    if (index === -1){
-        return res.status(404).json({ mensaje: "Cliente no encontrado" });
-    }
-    const { nombre, email, telefono, direccion, fechaRegistro } = req.body;
-    clientes[index] = { id, nombre, email, telefono, direccion, fechaRegistro };
-    res.json({ mensaje: "Cliente actualizado correctamente", cliente: clientes[index] });
-});
+router.put("/:id",actualizarCliente);
 
 
 //-------------------
 //Route DELETE
 //-------------------
 
-router.delete("/:id",(req,res)=> {
-    const id = parseInt(req.params.id); // Extrae el parámetro "id" de la URL y lo convierte a número entero. 
-    const index = clientes.findIndex((p) => p.id === id); 
-    if (index === -1) {
-    return res.status(404).json({ mensaje: "Cliente no encontrado" });
-    } 
-    const clienteEliminado = clientes.splice(index, 1);
-    res.json({ mensaje: "Cliente eliminado correctamente", cliente: clienteEliminado })
-});
-
+router.delete("/:id",eliminarCliente);
 
 
 
