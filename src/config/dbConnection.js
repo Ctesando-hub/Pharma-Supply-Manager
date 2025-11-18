88585ce6d20135627366f1d5f90324474faab04e
CarolinaT <27895429@ifts24.edu.ar>
2025-11-18 12:40:04 -0300

    resolviendo problemas con db

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -7,14 +7,6 @@
 // Importamos el paquete mysql2 con soporte de promesas
 import mysql from 'mysql2/promise';
 
-// Configuración de la conexión usando variables del .env
-const dbSettings = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-};
 
 // Función para obtener la conexión
 export async function getConnection() {
