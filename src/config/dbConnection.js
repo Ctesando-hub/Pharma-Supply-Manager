a8cb258671fe7d2833bd9ca5e7f6ca760e3ae579
CarolinaT <27895429@ifts24.edu.ar>
2025-11-18 12:51:29 -0300

    error de servidor 2

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -9,6 +9,13 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const dbSettings = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+};
 
 // Función para obtener la conexión
 export async function getConnection() {
